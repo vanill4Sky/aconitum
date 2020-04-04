5209fb75379ace2839fb1bf5b9f68d6361513b59
vanill4Sky <wojtek-s22@wp.pl>
2020-04-04 23:56:05 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui-SFML.h>
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+
+#include <entt.hpp>
+
+#include <sol/sol.hpp>
+
+int main()
+{
+    entt::registry registry;
+
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    lua.script_file("assets/scripts/test.lua");
+
+    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowTestWindow();
+
+        std::string res = lua["hello_world"]();
+        ImGui::Begin(res.c_str());
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return 0;
+}
