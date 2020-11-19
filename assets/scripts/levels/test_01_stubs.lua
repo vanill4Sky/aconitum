7557774cc705923190c7925946ce1e3ee583abc2
vanill4Sky <wojtek-s22@wp.pl>
2020-11-19 12:23:58 +0100

    Update test_01 level

--- assets/scripts/levels/test_01_stubs.lua
+++ assets/scripts/levels/test_01_stubs.lua
@@ -6,6 +6,10 @@ local stubs = {
 	player_0 = { idx = 1, name = "player", position = ex_vector2f:new(160.093, 155.464) },
 	box_1 = { idx = 2, name = "box", position = ex_vector2f:new(356.466, 94.9904) },
 	box_2 = { idx = 3, name = "box", position = ex_vector2f:new(357.399, 58.6028) },
+	box_3 = { idx = 4, name = "box", position = ex_vector2f:new(549.979, 200.322) },
+	box_4 = { idx = 5, name = "box", position = ex_vector2f:new(548.326, 224.289) },
+	box_6 = { idx = 6, name = "box", position = ex_vector2f:new(573.946, 197.017) },
+	box_5 = { idx = 7, name = "box", position = ex_vector2f:new(572.293, 224.289) },
 }
 
 function get_stubs()
